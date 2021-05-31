420ffa3f8a5176ab19c0b643068fbc6da234ac9b
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-01 01:10:55 +0200

    completo snack 1

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -1,11 +1,14 @@
-<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+<!--  Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
 
-Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.  -->
 
-// Apro tag per PHP
+<!-- Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<!-- // Apro tag per PHP -->
 <?php
 
-$teambasket = [
+$nba = [
     [
         "home" => "Falerian City Thunder",
         "away" => "Calcatesi Inibitis",
@@ -42,5 +45,33 @@ $teambasket = [
 
     ],
 ];
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack-1</title>
+</head>
+<body>
+    <ul>
+        <?php
+        for ($i = 0; $i < count($nba); $i++) {
+            $currentTeam = $nba[$i];
+            $teams= $currentTeam["home"] ." ". "VS" ." ". $currentTeam["away"];
+            $points= $currentTeam["homePoint"] ." ". "-" ." ". $currentTeam["awayPoint"];
+            ?>
+            <li>
+            <strong><?php echo $teams ?></strong>
+            <small><?php echo $points ?></small>
+            </li>
+            <?php
+
+        }
+        ?>
+
+    </ul>
+</body>
+</html>
