52827b69b812d7d7273fa5e7c880a3d449574dd0
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-01 01:00:38 +0200

    creo array con le partite

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -1,7 +1,46 @@
 <!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
 
-    Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
 
-Stampiamo a schermo tutte le partite con questo schema.
+// Apro tag per PHP
+<?php
 
-Olimpia Milano - Cantù | 55-60 -->
+$teambasket = [
+    [
+        "home" => "Falerian City Thunder",
+        "away" => "Calcatesi Inibitis",
+        "homePoint" => rand(1, 100),
+        "awayPoint" => rand(1, 100),
+
+    ],
+    [
+        "home" => "Rignano Seattles",
+        "away" => " L.A. Lakers",
+        "homePoint" => rand(1, 100),
+        "awayPoint" => rand(1, 100),
+
+    ],
+    [
+        "home" => "Utah Jazz",
+        "away" => "Dallas Mavericks",
+        "homePoint" => rand(1, 100),
+        "awayPoint" => rand(1, 100),
+        
+    ],
+    [
+        "home" => "San Antonio Spurs",
+        "away" => "Portland T. Blazers",
+        "homePoint" => rand(1, 100),
+        "awayPoint" => rand(1, 100),
+
+    ],
+    [
+        "home" => "Seattle S.Sonics",
+        "away" => "Phoenix Suns",
+        "homePoint" => rand(1, 100),
+        "awayPoint" => rand(1, 100),
+
+    ],
+];
+
+?>
