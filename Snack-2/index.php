f551b722e801727b644126e06f95015d67652737
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-01 01:16:51 +0200

    termino snack 2

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -6,4 +6,30 @@ $name = $_GET["name"];
 $age = $_GET["age"];
 $mail = $_GET["mail"];
 
+$validMail = true;
+
+if (strlen($name) < 4) {
+    $validMail = false;
+} else if (!is_numeric($age)) {
+    $validMail = false;
+} else if (strrpos($mail, "@") == false) {
+    $validMail = false;
+} else if (strrpos($mail, ".") == false) {
+    $validMail = false;
+}
+
+$result = $validMail ? 'Accesso riuscito' : 'Accesso negato';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div><?php echo $result; ?><div>
+</body>
+</html>
