6b4fa48d4f4aa7b8b260527a1cc435e268e70959
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-07 10:19:54 +0200

    impl snack 3 e 4

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -1,2 +1,87 @@
 <!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
+<?php
+/*
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+*/
+
+$posts = [
+   '10/01/2019' => [
+      [
+         'title' => 'Post 1',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 1'
+      ],
+      [
+         'title' => 'Post 2',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 2'
+      ],
+   ],
+   '10/02/2019' => [
+      [
+         'title' => 'Post 3',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 3'
+      ]
+   ],
+   '15/05/2019' => [
+      [
+         'title' => 'Post 4',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 4'
+      ],
+      [
+         'title' => 'Post 5',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 5'
+      ],
+      [
+         'title' => 'Post 6',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 6'
+      ]
+   ]
+];
+
+$postsKeys = array_keys($posts);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Snack 3</title>
+</head>
+<body>
+
+   <ul>
+      <?php
+      for ($i = 0; $i < sizeof($posts); $i++) {
+      ?>
+
+         <li>
+            <?php echo $postsKeys[$i]; ?>
+
+            <ul>
+
+               <?php
+               for ($x = 0; $x < sizeof($posts[$postsKeys[$i]]); $x++) {
+               ?>
+
+                  <li>
+                     <?php echo $posts[$postsKeys[$i]][$x]["title"] . "<br>" . $posts[$postsKeys[$i]][$x]["author"] . "<br>" . $posts[$postsKeys[$i]][$x]["text"] ?>
+                  </li>
+
+               <?php } ?>
+
+            </ul>
+         </li>
+
+      <?php } ?>
+   </ul>
+
+</body>
+</html>
--- Snack-4/index.php
+++ Snack-4/index.php
@@ -1,1 +1,45 @@
 <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+/*
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+*/
+
+$min = 1;
+$max = 100;
+$randomNumbers = 15;
+$array = [];
+
+while (sizeof($array) < $randomNumbers) {
+   $number = rand($min, $max);
+
+   if(!in_array($number, $array)) $array[] = $number;
+}
+
+sort($array);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Snack 4</title>
+</head>
+<body>
+
+   <ol>
+      <?php
+      for($i = 0; $i < sizeof($array); $i++) {
+      ?>
+
+         <li>
+            <?php echo $array[$i] ?>
+         </li>
+
+      <?php } ?>
+   </ol>
+
+</body>
+</html>
